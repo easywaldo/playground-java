929804fa324061a01265c05adf3b2a9286b3c564
easywaldoji <easywaldoji@gmail.com>
2020-05-19 12:18:56 +0900

    Thread Sample

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import model.OrderData;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -37,16 +42,64 @@ public class Main {
         orderList.add(orderData2);
 
 
+        System.out.println(result);
+        System.out.println(target.OrderId());
+        System.out.println(last.OrderId());
+        System.out.println(orderList.stream().count());
+
+
         //Interfaces Conflicted
         Employee empl = new Employee();
         System.out.println("empl's id : " + empl.getId());
         System.out.println("empl's secret id : " + empl.getSecretId());
 
-        System.out.println(result);
-        System.out.println(target.OrderId());
-        System.out.println(last.OrderId());
+
+        // Thread
+        Runnable helloTask = new HelloTask();
+        Thread helloThread = new Thread(helloTask);
+        helloThread.start();
+        System.out.println("thread is working...");
+
+        Runnable task = () -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("thread name : " + threadName);
+        };
+        task.run();
+        System.out.println("task is running..");
+        Thread thread = new Thread(task);
+        thread.start();
+        System.out.println("thread is starting..");
 
 
-        System.out.println(orderList.stream().count());
+        // Lambda
+        BiFunction<Integer, Integer, Integer> sum = (a, b) -> a + b;
+        System.out.println("lambda sample");
+        System.out.println(sum.apply(50, 50));
+        Comparator<Integer> myCompare = (a, b) -> {
+          if (a > b) {
+              return -1;
+          }
+          else if (a == b) {
+              return 0;
+          }
+          else {
+              return 1;
+          }
+        };
+        System.out.println(myCompare.compare(1,5));
+        System.out.println(myCompare.compare(4,2));
+
+        Predicate<Integer> myPred = (num) -> {
+            if (num > 1000) {
+                return  true;
+            }
+            return false;
+        };
+        System.out.println(myPred.test(1000));
+
+
+
+
+
     }
 }
